1e9e215966d5c3eb20a7f347acc938a0eaf6b809
Bruno <zettca@users.noreply.github.com>
2024-03-04 02:18:16 +0000

    chore: stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://recipes.zettca.xyz",
+  site: "https://zettca.xyz",
+  base: "/recipes-astro",
   integrations: [mdx(), sitemap()],
 });
