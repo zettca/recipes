6879bc917804fd0c5cf932db1415bcd06e52bbc2
Bruno <zettca@users.noreply.github.com>
2024-03-10 17:30:48 +0000

    fix: style stuff

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,14 @@
 // uno.config.ts
 import { defineConfig, presetUno, presetTypography } from "unocss";
 import type { Theme } from "unocss/preset-uno";
+import presetIcons from "@unocss/preset-icons";
 import presetTheme from "unocss-preset-theme";
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetTypography(),
+    presetIcons(),
     presetTheme<Theme>({
       prefix: "uno-",
       theme: {
