711a4d545816bb96647002f3366747605d285142
Bruno <zettca@users.noreply.github.com>
2024-05-22 18:30:10 +0100

    fix: allow recipes schema null

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,17 @@ import { defineCollection, z } from "astro:content";
 
 const recipes = defineCollection({
   type: "content",
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    description: z.string().optional().default(""),
-    tags: z.string().optional(),
-    author: z.string().optional(),
+    description: z.string().optional().nullable().default(""),
+    tags: z.string().optional().nullable(),
+    author: z.string().optional().nullable(),
     /** hidden in the homepage */
     hidden: z.boolean().default(false),
     lang: z.string().default("en"),
     // Transform string to Date object
     pubDate: z.coerce.date(),
-    heroImage: z.string().optional(),
+    heroImage: z.string().optional().nullable(),
   }),
 });
 
