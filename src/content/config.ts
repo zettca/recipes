0314cc83cfc977e09d0fc0163afefb6a8e4ccd56
Bruno <zettca@users.noreply.github.com>
2024-05-22 19:08:11 +0100

    fix: schema nullish params

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,15 +4,18 @@ const recipes = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    description: z.string().optional().nullable().default(""),
-    tags: z.string().optional().nullable(),
-    author: z.string().optional().nullable(),
+    description: z
+      .string()
+      .nullish()
+      .transform((s) => s || ""),
+    tags: z.string().nullish(),
+    author: z.string().nullish(),
     /** hidden in the homepage */
     hidden: z.boolean().default(false),
     lang: z.string().default("en"),
     // Transform string to Date object
     pubDate: z.coerce.date(),
-    heroImage: z.string().optional().nullable(),
+    heroImage: z.string().nullish(),
   }),
 });
 
